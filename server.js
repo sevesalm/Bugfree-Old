8901304dc6645b215ccba57dec1cbf67a6d11b9a
Severi Salminen <severi.salminen@iki.fi>
2017-03-24 14:56:51 +0200

    Changed to use promises and arrow functions.

--- server.js
+++ server.js
@@ -8,32 +8,32 @@ var mongoDB = null;
 
 app.use(express.static(__dirname))
 app.set('view engine', 'pug');
-app.get('/', function(req, res) {
+
+app.get('/', (req, res) => {
     res.render('main');
 });
 
-app.get('/projects/', function(req, res) {
-	mongoDB.collection('projects').find().toArray(function(err, projects) {
+app.get('/projects/', (req, res) => {
+	mongoDB.collection('projects').find().toArray( (err, projects) => {
     	res.render('projects', {projects});
 	});
 });
 
-app.get('/profile/', function(req, res) {
+app.get('/profile/', (req, res) => {
     res.render('profile');
 });
 
-MongoClient.connect(mongoUrl, function(err, db) {
-  if(!err) {
-    console.log("MongoDB: connected");
-    mongoDB = db;
+MongoClient.connect(mongoUrl)
+  .then(function(db) {
+      console.log("MongoDB: connected");
+      mongoDB = db;
 
-    var port = 3001;
-    app.listen(port, function() {
-    	console.log('Bugfree: listening on localhost: %d', port);
-    });
-  }
-  else {
-    console.log("MongoDB: error connecting!");
-    console.log("err.message: ", err.message);
-  }
-});
+      var port = 3001;
+      app.listen(port, () => {
+        console.log('Bugfree: listening on localhost: %d', port);
+      });
+  })
+  .catch((err) => {
+      console.log("MongoDB: error connecting!");
+      console.log("err.message: ", err.message);
+  });
