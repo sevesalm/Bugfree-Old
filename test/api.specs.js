f021723ac456ef8588c19b9333de040cba9b7c12
Severi Salminen <severi.salminen@iki.fi>
2017-07-29 01:28:36 +0300

    Fixed and refactored api tests

--- test/api.specs.js
+++ test/api.specs.js
@@ -1,37 +1,37 @@
-var chai = require('chai');
-var should = chai.should();
-var chaiHttp = require('chai-http');
-var server = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
 
 chai.use(chaiHttp);
+const expect = chai.expect;
 
 describe('API routes', () => {
-    it('should return all projects', () => {
-        return chai.request('http://localhost:3001').get('/api/projects')
-        .then(res => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-            return res.body.should.have.length(5);
-        });
-    });
+  it('should return all projects', () =>
+    chai.request(server).get('/api/projects')
+    .then(res => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.a('array');
+      expect(res.body).to.have.length(5);
+      return expect(res).to.be.json;
+    })
+    );
 
-    it('should return a single project', () => {
-        return chai.request('http://localhost:3001').get('/api/projects/1')
-        .then(res => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.include.keys('id');
-            res.body.id.should.equal('1');
-            return res.body.title.should.equal('AgriScore');
-        });
-    });
+  it('should return a single project', () =>
+    chai.request(server).get('/api/projects/1')
+    .then(res => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.include.keys('id');
+      expect(res.body.id).to.equal(1);
+      expect(res.body.title).to.equal('AgriScore');
+      return expect(res).to.be.json;
+    })
+  );
 
-    it('should return redirect to /login', () => {
-        return chai.request('http://localhost:3001').get('/publish')
-        .then(res => {
-            res.redirects[0].should.equal('http://localhost:3001/login/');
-            return res.should.have.status(200);
-        });
-    });
+  it('should return redirect to /login', () =>
+    chai.request(server).get('/publish')
+    .then(res => {
+      expect(res).to.have.status(200);
+      return expect(res).to.redirectTo('http://127.0.0.1:3001/login/');
+    })
+  );
 });
