8690c4045170246fd3489690015ffa76d26bc6d1
Severi Salminen <severi.salminen@iki.fi>
2017-07-30 21:23:19 +0300

    Add development environment

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'bugfree',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+  },
   production: {
     client: 'pg',
     connection: {
