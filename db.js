73bf65a29ef8c8326356ca349b9842294f9a4d59
Severi Salminen <severi.salminen@iki.fi>
2017-07-30 14:13:18 +0300

    Add basic Travis config

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const knexfile = require('./knexfile.js');
-const knex = require('knex')(knexfile.development);
+const knex = require('knex')(knexfile[process.env.NODE_ENV]);
 
 knex.migrate.latest([knexfile])
   .catch(err => console.log(err));
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,18 @@
 module.exports = {
-  development: {
+  production: {
     client: 'pg',
     connection: {
       database: 'bugfree',
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+    },
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'bugfree_test',
+      user: 'postgres',
+      password: '',
     },
   },
 };
