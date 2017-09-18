2ef8171fe5b6a4387f95f4e0e6f48a65ed086a59
Severi Salminen <severi.salminen@iki.fi>
2017-09-18 23:53:46 +0300

    Implement modal without Bootstrap

--- static/script.js
+++ static/script.js
@@ -1,23 +1,16 @@
 // Open modal on any image click
 $(function() {
+
   $('body').on('click', 'img.can-open', function () {
-    const $img = $('<img>').addClass('img-responsive').attr('alt', 'Image preview').attr('src', $(this).attr('src'));
-    $('.modal-body').append($img);
-    $('#image-modal').modal('show');
+    // const $img = $('<img>').addClass('modal__image').attr('alt', 'Image preview').attr('src', $(this).attr('src'));
+    // $('.modal__body').html($img);
+    const $img = $('<div class="modal__image">').css('background-image', 'url(' + $(this).attr('src') + ')');
+    $('.modal__body').html($img);
+    $('#image-modal').show();
   });
   // Close modal on click
   $('#image-modal').on('click', function () {
-    $('#image-modal').modal('hide');
-  });
-
-  $('#image-modal').on('hidden.bs.modal', function () {
-    $('.modal-body').empty();
-  });
-
-  $('.section').hover(function () {
-    $('i', this).addClass('hovering');
-  }, function () {
-    $('i', this).removeClass('hovering');
+    $('#image-modal').hide();
   });
 
   $('.my-navbar__toggle').click(function () {
